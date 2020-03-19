e4778362df4e83ba76a8a84ee584fda55c581fdc
Peet <peetinc@users.noreply.github.com>
2020-03-19 06:54:10 -0600

    hmmm

--- munki-enroll/enroll-update.php
+++ munki-enroll/enroll-update.php
@@ -1,20 +1,26 @@
 <?php
 namespace CFPropertyList;
 
-// require cfpropertylist
+// require cfpropertylist and logger
 require_once 'cfpropertylist-2.0.1/CFPropertyList.php';
 require_once 'logger.php';
 
-// Manifest Relative path (include trailing / i.e. '../manifests/')
-$manifestspath = '../manifests/';
-
-//Set default manifest and catalog
+//////// All Variables
+//// Start User Variables
+// User-set variables
+// Provide default manifest and catalog
 $defaultmanifest = 'Management/Mandatory';
 $defaultcatalog = 'production';
+////
+// Munki Repo Varibles
+// Manifest and catalogs relative path (include trailing / i.e. '../manifests/')
+$manifestspath = '../manifests/';
+$catalogspath = '../catalogs/';
+//// End User Variables
 
-// Get the varibles passed by the enroll script
+// Variables provided by munki-enroll.sh
 //if $function is not passed, set to enroll
-if (!($recordname = filter_input(INPUT_GET, 'function', FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES)))
+if (!($function = filter_input(INPUT_GET, 'function', FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES)))
 	{
 		$function = 'enroll'; 
 	}
@@ -45,8 +51,9 @@ $catalog2 = filter_input(INPUT_GET, 'catalog2', FILTER_SANITIZE_STRING, FILTER_F
 $catalog3 = filter_input(INPUT_GET, 'catalog3', FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
 $uuid = filter_input(INPUT_GET, 'uuid', FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
 
-function varCheck($recordname, $displayname, $uuid, $catalog1, $catalog2, $catalog3, $manifest1, $manifest2, $manifest3, $manifest4) {
-// end if no variables provided
+//// Functions
+function minVarCheck($recordname, $displayname, $uuid, $catalog1, $catalog2, $catalog3, $manifest1, $manifest2, $manifest3, $manifest4) {
+// end if not enough variables provided
 if ( $recordname == "_NOT-PROVIDED_" or $displayname == "_NOT-PROVIDED_" or $uuid = "_NOT-PROVIDED_" )
 	{
 		echo "Please provide recordname, displayname and uuid at minimum.\n";
@@ -58,19 +65,68 @@ if ( $recordname == "_NOT-PROVIDED_" or $displayname == "_NOT-PROVIDED_" or $uui
 	}
 }
 
+// Ensure provided catalogs and manifests exist.
+function varCheck($recordname, $displayname, $uuid, $catalog1, $catalog2, $catalog3, $manifest1, $manifest2, $manifest3, $manifest4) {
+if ( !file_exists( $catalogspath . $catalog1 ) )
+	{
+	echo "$catalog1 does not exist... \n";
+	$exit = '999';
+	}
+if ( !file_exists( $catalogspath . $catalog2 ) )
+	{
+	echo "$catalog1 does not exist... \n";
+	$exit = '999';
+	}
+if ( !file_exists( $catalogspath . $catalog3 ) )
+	{
+	echo "$catalog1 does not exist... \n";
+	$exit = '999';
+	}
+if ( !file_exists( $manifestspath . $manifest1 ) )
+	{
+	echo "$manifest1 does not exist... \n";
+	$exit = '999';
+	}
+if ( !file_exists( $manifestspath . $manifest2 ) )
+	{
+	echo "$manifest2 does not exist... \n";
+	$exit = '999';
+	}
+if ( !file_exists( $manifestspath . $manifest3 ) )
+	{
+	echo "$manifest3 does not exist... \n";
+	$exit = '999';
+	}
+if ( !file_exists( $manifestspath . $manifest4 ) )
+	{
+	echo "$manifest4 does not exist... \n";
+	$exit = '999';
+	}
+if ( $exit == 999 )
+	{
+	echo "exit \n";
+	echo "999";
+	exit(999);
+	} else {
+	echo "Provided catalog(s) and manifest(s) exist. So there's that... \n\n";
+	}
+}
+
+// Needs to be fixed for $function=update or $function=enroll
 function existingRecordCheck($recordname, $displayname, $uuid, $catalog1, $catalog2, $catalog3, $manifest1, $manifest2, $manifest3, $manifest4) {
 // Check if manifest already exists for this machine
-echo "MUNKI-ENROLL. Checking for existing manifests.\n\n";
+echo "MUNKI-ENROLL. Checking for existing manifests... \n\n";
 
-if ( file_exists( $manifestspath . $recordname ) )
+if ( file_exists( $manifestspath . $recordname ) && $function == 'enroll' )
 	{
-		echo "Computer manifest " . $recordname . " already exists.\n";
-		echo "You're trying to be naughty.\n";
+	echo "Computer manifest " . $recordname . " already exists... \n";
+		echo "You're trying to be naughty... \n";
 		echo "9";
 		$result = 'FAILURE - EXISTING MANIFEST';
 		logger($result, $recordname, $displayname, $catalog1, $catalog2, $catalog3, $manifest1, $manifest2, $manifest3, $manifest4);
 		exit(9);
     }
+   
 }
 
 function nestCheck($recordname, $displayname, $uuid, $catalog1, $catalog2, $catalog3, $manifest1, $manifest2, $manifest3, $manifest4) {
